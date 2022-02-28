a09da125269eff6a7666db8addebd16fbee3a94d
Alex Ho <vnitus@gmail.com>
2022-02-28 15:44:08 -0800

    Improve the logic to support different API response format for each vendor Add test cases

--- app/Http/Resources/OrderLineItemResource.php
+++ app/Http/Resources/OrderLineItemResource.php
@@ -14,8 +14,46 @@ class OrderLineItemResource extends JsonResource
      */
     public function toArray($request)
     {
+        $user = $request->user();
+
+        $vendor = $user->vendor;
+
+        // Return different format of Order Line Item for each Vendor
+        return match ($vendor->api_format) {
+            'marco_fine_arts' => $this->formatMarcoFineArts(),
+            default => $this->formatDefault(),
+        };
+    }
+
+    /**
+     * Custom format of Order Line Item resource for Marco Fine Arts vendor
+     *
+     * @return array
+     */
+    private function formatMarcoFineArts()
+    {
         return [
-            'external_order_line_item_id' => $this->id,
+            'external_order_line_item_id' => strval($this->id), // They want string instead of number for this field
+            'product_id' => strval($this->product_id), // They want string instead of number for this field
+            'quantity' => strval($this->quantity), // They want string instead of number for this field
+            'image_url' => $this->product()->exists() ?
+                ($this->product->creative()->exists() ?
+                    $this->product->creative->image_url
+                    : null
+                )
+                : null,
+        ];
+    }
+
+    /**
+     * Custom format of Order Line Item resource for other vendors
+     *
+     * @return array
+     */
+    private function formatDefault()
+    {
+        return [
+            'order_line_item_id' => $this->id,
             'product_id' => $this->product_id,
             'quantity' => $this->quantity,
             'image_url' => $this->product()->exists() ?
--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\OrderLineItem;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderResource extends JsonResource
@@ -18,8 +19,23 @@ class OrderResource extends JsonResource
 
         $vendor = $user->vendor;
 
+        // Return different format of Order for each Vendor
+        return match ($vendor->api_format) {
+            'marco_fine_arts' => $this->formatMarcoFineArts($vendor),
+            default => $this->formatDefault($vendor),
+        };
+    }
+
+    /**
+     * Custom format of Order resource for Marco Fine Arts vendor
+     *
+     * @param $vendor
+     * @return array
+     */
+    private function formatMarcoFineArts($vendor)
+    {
         return [
-            'external_order_id' => $this->id,
+            'external_order_id' => strval($this->id), // They want string instead of number for this field
             'buyer_first_name' => $this->first_name,
             'buyer_last_name' => $this->last_name,
             'buyer_shipping_address_1' => $this->address_1,
@@ -28,7 +44,35 @@ class OrderResource extends JsonResource
             'buyer_shipping_state' => $this->state,
             'buyer_shipping_postal' => $this->postal_code,
             'buyer_shipping_country' => $this->country,
-            'print_line_items' => OrderLineItemResource::collection($vendor->orderLineItems),
+            'print_line_items' => OrderLineItemResource::collection(OrderLineItem::where([
+                'vendor_id' => $vendor->id,
+                'order_id' => $this->id
+            ])->get()),
+        ];
+    }
+
+    /**
+     * Custom format of Order resource for other vendors
+     *
+     * @param $vendor
+     * @return array
+     */
+    private function formatDefault($vendor)
+    {
+        return [
+            'order_id' => $this->id,
+            'buyer_first_name' => $this->first_name,
+            'buyer_last_name' => $this->last_name,
+            'buyer_shipping_address_1' => $this->address_1,
+            'buyer_shipping_address_2' => $this->address_2,
+            'buyer_shipping_city' => $this->city,
+            'buyer_shipping_state' => $this->state,
+            'buyer_shipping_postal' => $this->postal_code,
+            'buyer_shipping_country' => $this->country,
+            'order_line_items' => OrderLineItemResource::collection(OrderLineItem::where([
+                'vendor_id' => $vendor->id,
+                'order_id' => $this->id
+            ])->get()),
         ];
     }
 }
